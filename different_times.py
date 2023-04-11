06a0edc842ea4d1821a29a01943d5c482d3882e3
Jasmine Zhang <jasminez@princeton.edu>
2023-04-10 22:11:29 -0400

    progress for different room times but wrong. think about recreating roomTimeDict

--- different_times.py
+++ different_times.py
@@ -1,16 +1,20 @@
 import pulp as pl
 import numpy as np
-#import pytups as pt
 
 classes = ['A', 'B', 'C', 'D', 'E']
-rooms = [1, 2, 3]
+rooms = [1, 2, 3, 4]
 days = ['Mon', 'Tues', 'Wed', 'Thur', 'Fri']
 classTimeSlots = [1,2,3]
 roomTimeSlots = [1,2,3]
 # assume for now all the rooms have the correct resources for all the classes
 
+# class --> time slot class can be scheduled at
 classTimeDict = {'A': [1,2,3], 'B': [1], 'C': [1,2,3], 'D': [1,2], 'E': [2,3]}
-roomTimeDict = {1:[1,2,3], 2:[1], 3:[1,2]}
+# room --> time slot room can be scheduled at
+roomTimeDict = {1:[1,2,3], 2:[1], 3:[1,2], 4: [3]}
+numRooms = len(roomTimeDict)
+numTimes = max(max(roomTimeDict.values()))
+Z = np.zeros((numRooms,numTimes))
 
 sched = pl.LpProblem("Course Scheduling")
 
@@ -18,53 +22,20 @@ C = len(classes)
 T = len(classTimeSlots)
 R = len(rooms)
 
-for i in classTimeDict:
-    for j in classTimeDict[i]:
-        print(i, " ", j)
+for i in roomTimeDict:
+    for j in roomTimeDict[i]:
+        Z[i-1][j-1] = 1
+
+print(Z)
 
 classAssignments = [(c,t) for c in classTimeDict for t in classTimeDict[c]] 
 
-# this will need to be adjusted for variable time slots open
-# need to split this up. get the values for every classtime, then let that be the second argument
-# can i build up cVars
-
-
-classes = ['A', 'B', 'C', 'D', 'E']
-classTimeSlots = [1,2,3]
-classTimeDict = {'A': [1,2,3], 'B': [1], 'C': [1,2,3], 'D': [1,2], 'E': [2,3]}
-
-k = []
-v = []
-
-# for i in classTimeDict:
-#     for j in classTimeDict[i]:
-#         k.append(i)
-#         v.append(j)
 
 cVars = {}
 
 for i in classTimeDict:
     up = pl.LpVariable.dicts("Class Assignment", (i, classTimeDict[i]), 0, None, pl.LpInteger)
-    # print("!!!!!!!")
-    # print(cVars)
     cVars.update(up)
-
-print("!!!!!!AAAAAAAAAA!!!!!!")
-print(cVars)
-
-#grr = pt.TupList((t, p) for t in bgh for p in agh)
-#print(grr)
-
-#cVars = pl.LpVariable.dicts("Class Assignment", ((classTimeDict, classTimeDict[c]) for c in classTimeDict.keys()), 0, None, pl.LpInteger) 
-#cVars = pl.LpVariable.dicts("Class Assignment", (classTimeDict, classTimeSlots), 0, None, pl.LpInteger)
-
-#cVars = pl.LpVariable.dicts("Class Assignment", ((bgh, agh[c]) for c in range(len(bgh))), 0, None, pl.LpInteger)
-#cVars = pl.LpVariable.dicts("Class Assignment", (k, v), 0, None, pl.LpInteger)
-
-#cVars = pl.LpVariable.dicts("Class Assignment", ((k[i],v[i]) for i in range(len(k))), 0, None, pl.LpInteger)
-
-# DESIRED CVARS: {'A': {1: Class_Assignment_A_1, 2: Class_Assignment_A_2, 3: Class_Assignment_A_3}, 'B': {1: Class_Assignment_B_1,}, 'C': {1: Class_Assignment_C_1, 2: Class_Assignment_C_2, 3: Class_Assignment_C_3}, 'D': {1: Class_Assignment_D_1, 2: Class_Assignment_D_2}, 'E': {2: Class_Assignment_E_2, 3: Class_Assignment_E_3}}
-# CURRENT CVARS: {('A', 1): Class_Assignment_('A',_1), ('A', 2): Class_Assignment_('A',_2), ('A', 3): Class_Assignment_('A',_3), ('B', 1): Class_Assignment_('B',_1), ('C', 1): Class_Assignment_('C',_1), ('C', 2): Class_Assignment_('C',_2), ('C', 3): Class_Assignment_('C',_3), ('D', 1): Class_Assignment_('D',_1), ('D', 2): Class_Assignment_('D',_2), ('E', 2): Class_Assignment_('E',_2), ('E', 3): Class_Assignment_('E',_3)}
 
 
 roomAssignments = [(c,r) for c in classTimeDict for r in roomTimeDict]
@@ -91,14 +62,22 @@ for c1 in classTimeDict:
         for t in classTimeDict[c1]:
             for r in roomTimeDict:
                 if(c1==c2): continue
-                # print("testing", cVars[c2])
-                # print("testing2",cVars[c2][t])
-                # print(cVars[c2].get(t))
                 if(cVars[c2].get(t) == None): 
                     continue
                 sched += (
                     cVars[c1][t] + cVars[c2][t] + rVars[c1][r] + rVars[c2][r] <=3
                 )
+
+
+for c in classTimeDict:
+    for t in classTimeDict[c]:
+        for r in roomTimeDict:
+            print(t not in roomTimeDict[r])
+            if(t not in roomTimeDict[r]): 
+                continue
+            sched += (
+                cVars[c][t] + rVars[c][r] - 1 <= Z[r-1][t-1]
+            )
 
 # The problem data is written to an .lp file
 sched.writeLP("SchedulingProblem.lp")
